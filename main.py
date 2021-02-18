e9cbbe6969ec4c66565be59667a0c1ebe20f8900
KennyV91 <velasco_ariel_123@hotmail.com>
2021-02-17 23:47:11 -0400

    pruebas de figuras

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ w, h = letter
 c = canvas.Canvas("Recibo-Saval.pdf", pagesize=letter)
 c.drawString(50, h-50, "Saval SRL")
 
-
+x = 50
+y = h -50
+c.line(x, y, x + 200, y)
+c.rect(50, h - 300, 300, 200)
+c.roundRect(50, h - 300, 300, 200, 10)
+c.circle(100, h - 100, 50)
+c.ellipse(50, h - 50, x + 150, y - 50)
 
 c.save()
